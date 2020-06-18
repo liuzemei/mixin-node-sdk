0f7b36655e01ecfadb022718fa71ad1fd8bf295a
neo <neo@Mac-mini.local>
2020-06-18 17:29:22 +0800

    socket fixed

--- MixinSocket.js
+++ MixinSocket.js
@@ -10,75 +10,54 @@ class MixinSocket extends Mixin {
     this.url = useChinaServer ? 'wss://mixin-blaze.zeromesh.net' : 'wss://blaze.mixin.one/'
     this.protocols = 'Mixin-Blaze-1'
     this.debug = debug || false
-    this.wsInterval = null
     this.ws = null
+    this.isAlive = false
+    this.pingInterval = 0
   }
 
   start() {
-    if (this.ws && this.ws.readyState === WebSocket.CONNECTING) return
-    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      this.ws.close(1000)
-      this.ws = null
-    }
-
     const headers = {
       Authorization: `Bearer ${this.getJwtToken(this.CLIENT_CONFIG, 'GET', '/', '')}`
     }
-    this.ws = new WebSocket(this.url, this.protocols, { headers })
+    this.ws = new WebSocket(this.url, this.protocols, { headers, handshakeTimeout: 3000 })
     this.ws.onmessage = this._on_message.bind(this)
     this.ws.onerror = this._on_error.bind(this)
     this.ws.onclose = this._on_close.bind(this)
     this.ws.onopen = this._on_open.bind(this)
 
-    clearInterval(this.wsInterval)
-    this.wsInterval = setInterval(() => {
-      if (this.ws && this.ws.readyState !== WebSocket.OPEN) {
-        this.start()
-      }
-    }, 15000)
-  }
+    this.ws.on('pong', () => {
+      this.isAlive = true
+    })
 
-  send_raw(message) {
-    return new Promise((resolve) => {
-      try {
-        const buffer = Buffer.from(JSON.stringify(message), 'utf-8');
-        zlib.gzip(buffer, (_, zipped) => {
-          if (this.ws.readyState === WebSocket.OPEN) {
-            this.ws.send(zipped);
-            resolve(true);
-          } else {
-            resolve(false);
-          }
-        });
-      } catch (e) {
-        resolve(false);
-      }
-    });
-  }
-
-  async get_message_handler(message) { }
-
-  async  _on_message(originData) {
-    let message = await this.decode(originData.data)
-    if (message === false) return this.ws.readyState !== WebSocket.OPEN && this.start()
-    await this.get_message_handler(message)
+    this.pingInterval = setInterval(() => {
+      if (this.ws.readyState === WebSocket.CONNECTING) return
+      if (!this.isAlive) return this.ws.terminate()
+      this.isAlive = false
+      this.ws.ping()
+    }, 1000 * 30)
   }
 
   _on_open() {
     if (this.debug) console.error('ws connected...' + new Date().toISOString())
+    this.isAlive = true
     this.send_raw({ id: this.getUUID(), action: 'LIST_PENDING_MESSAGES' })
   }
 
+  _on_close(e) {
+    clearInterval(this.pingInterval)
+    this.start()
+  }
 
   _on_error(e) {
     if (this.debug) console.error(e)
   }
 
-  _on_close(e) {
-    if (this.debug) console.error(e)
-    this.start()
+  async _on_message(originData) {
+    let message = await this.decode(originData.data)
+    await this.get_message_handler(message)
   }
 
+  async get_message_handler(message) { }
 
   async read_message(message) {
     if (!message || !message.data || !message.data.message_id) return false
@@ -90,25 +69,6 @@ class MixinSocket extends Mixin {
         status: 'READ'
       }
     })
-  }
-
-  decode(data) {
-    return new Promise((resolve) => {
-      try {
-        zlib.gunzip(data, (err, unzipped) => {
-          if (err) {
-            return resolve(false)
-          }
-          const msgObj = JSON.parse(unzipped.toString());
-          if (msgObj && msgObj.action === 'CREATE_MESSAGE' && msgObj.data && msgObj.data.category === 'PLAIN_TEXT') {
-            msgObj.data.data = Buffer.from(msgObj.data.data, 'base64').toString('utf-8');
-          }
-          resolve(msgObj);
-        });
-      } catch (e) {
-        return resolve(false)
-      }
-    });
   }
 
   async send_text(text, message) {
@@ -148,6 +108,17 @@ class MixinSocket extends Mixin {
     return await this.send_raw(this._create_message(obj, message, 'APP_CARD'))
   }
 
+  async send_message(data, message, type) {
+    return await this.send_raw(this._create_message(data, message, type))
+  }
+
+  async send_messages(messages) {
+    return await this.send_raw({
+      id: this.getUUID(),
+      action: "CREATE_PLAIN_MESSAGES",
+      params: { messages }
+    })
+  }
 
   _create_message(data, message, type) {
     data = typeof data === 'object' ? JSON.stringify(data) : data.toString()
@@ -164,16 +135,41 @@ class MixinSocket extends Mixin {
     }
   }
 
-  async send_message(data, message, type) {
-    return await this.send_raw(this._create_message(data, message, type))
+  decode(data) {
+    return new Promise((resolve) => {
+      try {
+        zlib.gunzip(data, (err, unzipped) => {
+          if (err) {
+            return resolve(false)
+          }
+          const msgObj = JSON.parse(unzipped.toString());
+          if (msgObj && msgObj.action === 'CREATE_MESSAGE' && msgObj.data && msgObj.data.category === 'PLAIN_TEXT') {
+            msgObj.data.data = Buffer.from(msgObj.data.data, 'base64').toString('utf-8');
+          }
+          resolve(msgObj);
+        });
+      } catch (e) {
+        return resolve(false)
+      }
+    });
   }
 
-  async send_messages(messages) {
-    return await this.send_raw({
-      id: this.getUUID(),
-      action: "CREATE_PLAIN_MESSAGES",
-      params: { messages }
-    })
+  send_raw(message) {
+    return new Promise((resolve) => {
+      try {
+        const buffer = Buffer.from(JSON.stringify(message), 'utf-8');
+        zlib.gzip(buffer, (_, zipped) => {
+          if (this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(zipped);
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        });
+      } catch (e) {
+        resolve(false);
+      }
+    });
   }
 }
 
