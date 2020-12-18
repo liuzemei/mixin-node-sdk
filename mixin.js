4c4677d7c2043bf176d6a3caf7fc7e2eb3b68ff1
Neo <her_liu@qq.com>
2020-12-18 11:29:11 +0800

    add ed25519

--- mixin.js
+++ mixin.js
@@ -1,9 +1,8 @@
 const forge = require('node-forge');
 const jwt = require('jsonwebtoken');
-const { Uint64BE } = require('int64-buffer');
+const { Int64BE } = require('int64-buffer');
 const crypto = require('crypto');
-
-
+const crypto_scalarmult = require('./ed25519')
 class MixinBase {
   constructor(config) {
     this.CLIENT_CONFIG = config
@@ -59,13 +58,13 @@ function _getUUID() {
 }
 
 function signAuthenticationToken(uid, sid, privateKey, method, uri, body, scp) {
-  uri = uri.replace('https://api.mixin.one', '')
+  if (uri.startsWith('https://api.mixin.one')) uri = uri.replace('https://api.mixin.one', '')
+  if (uri.startsWith('https://mixin-api.zeromesh.net')) uri = uri.replace('https://mixin-api.zeromesh.net', '')
   method = method.toLocaleUpperCase();
-  if (typeof (body) === "object") {
-    body = JSON.stringify(body);
-  }
+  if (typeof (body) === "object") body = JSON.stringify(body);
   let issuedAt = Math.floor(Date.now() / 1000)
   let md = forge.md.sha256.create();
+  let _privateKey = toBuffer(privateKey, 'base64');
   md.update(method + uri + body, 'utf8');
   let payload = {
     uid: uid,
@@ -76,24 +75,44 @@ function signAuthenticationToken(uid, sid, privateKey, method, uri, body, scp) {
     sig: md.digest().toHex(),
     scp: scp || 'FULL'
   };
-  return jwt.sign(payload, privateKey, { algorithm: 'RS512' });
+  return _privateKey.length === 64 ? getEd25519Sign(payload, _privateKey) : jwt.sign(payload, privateKey, { algorithm: 'RS512' });
+}
+
+function getEd25519Sign(payload, privateKey) {
+  const header = toBuffer({ alg: "EdDSA", typ: "JWT" }).toString('base64')
+  payload = base64url(toBuffer(payload))
+  const result = [header, payload]
+  const sign = base64url(forge.pki.ed25519.sign({
+    message: result.join('.'),
+    encoding: 'utf8',
+    privateKey
+  }))
+  result.push(sign)
+  return result.join('.')
+}
+
+function toBuffer(content, encoding = 'utf8') {
+  if (typeof content === 'object') content = JSON.stringify(content)
+  return Buffer.from(content, encoding)
+}
+
+function base64url(buffer) {
+  return buffer.toString('base64').replace(/\=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
 
 
 function signEncryptedPin(pin, pinToken, sessionId, privateKey, iterator) {
   const blockSize = 16;
-  pinToken = Buffer.from(pinToken, 'base64');
-  privateKey = forge.pki.privateKeyFromPem(privateKey);
-  let pinKey = privateKey.decrypt(pinToken, 'RSA-OAEP', {
-    md: forge.md.sha256.create(),
-    label: sessionId
-  });
-  let time = new Uint64BE(Math.floor(Date.now() / 1000));
+
+  let _privateKey = toBuffer(privateKey, 'base64');
+  let pinKey = _privateKey.length === 64 ? signEncryptEd25519PIN(pinToken, _privateKey) : signPin(pinToken, privateKey, sessionId)
+
+  let time = new Int64BE(Date.now() / 1000 | 0);
   time = [...time.toBuffer()].reverse();
   if (iterator == undefined || iterator === "") {
     iterator = Date.now() * 1000000;
   }
-  iterator = new Uint64BE(iterator);
+  iterator = new Int64BE(iterator);
   iterator = [...iterator.toBuffer()].reverse();
   pin = Buffer.from(pin, 'utf8');
   let buf = Buffer.concat([pin, Buffer.from(time), Buffer.from(iterator)]);
@@ -104,7 +123,7 @@ function signEncryptedPin(pin, pinToken, sessionId, privateKey, iterator) {
   }
   buf = Buffer.concat([buf, Buffer.from(paddingArray)]);
   let iv16 = crypto.randomBytes(16);
-  let cipher = crypto.createCipheriv('aes-256-cbc', hexToBytes(forge.util.binary.hex.encode(pinKey)), iv16);
+  let cipher = crypto.createCipheriv('aes-256-cbc', pinKey, iv16);
   cipher.setAutoPadding(false);
   let encrypted_pin_buff = cipher.update(buf, 'utf-8');
   encrypted_pin_buff = Buffer.concat([iv16, encrypted_pin_buff]);
@@ -112,13 +131,48 @@ function signEncryptedPin(pin, pinToken, sessionId, privateKey, iterator) {
 }
 
 
-function hexToBytes(hex) {
-  var bytes = [];
-  for (let c = 0; c < hex.length; c += 2) {
-    bytes.push(parseInt(hex.substr(c, 2), 16));
-  }
-  return Buffer.from(bytes);
+function signPin(pinToken, privateKey, sessionId) {
+  pinToken = Buffer.from(pinToken, 'base64');
+  privateKey = forge.pki.privateKeyFromPem(privateKey);
+  let pinKey = privateKey.decrypt(pinToken, 'RSA-OAEP', {
+    md: forge.md.sha256.create(),
+    label: sessionId
+  });
+  return hexToBytes(forge.util.binary.hex.encode(pinKey))
 }
 
+function signEncryptEd25519PIN(pinToken, privateKey) {
+  pinToken = Buffer.from(pinToken, 'base64')
+  return scalarMult(privateKeyToCurve25519(privateKey), pinToken.slice(0, 32))
+}
+
+function scalarMult(curvePriv, publicKey) {
+  curvePriv[0] &= 248
+  curvePriv[31] &= 127
+  curvePriv[31] |= 64
+  var sharedKey = new Uint8Array(32);
+  crypto_scalarmult(sharedKey, curvePriv, publicKey);
+  return sharedKey;
+}
+
+function privateKeyToCurve25519(privateKey) {
+  const seed = privateKey.slice(0, 32)
+  const sha512 = crypto.createHash('sha512')
+  sha512.write(seed, 'binary')
+  let digest = sha512.digest()
+  digest[0] &= 248
+  digest[31] &= 127
+  digest[31] |= 64
+  return digest.slice(0, 32)
+}
+
+function hexToBytes(hex) {
+  const bytes = new Uint8Array(32);
+  let i = 0
+  for (let c = 0; c < hex.length; c += 2) {
+    bytes[i++] = parseInt(hex.substr(c, 2), 16)
+  }
+  return bytes
+}
 
 module.exports = MixinBase
